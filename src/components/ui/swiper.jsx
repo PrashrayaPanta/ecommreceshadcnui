7e6258d7a246109f4869a458186fdacce9cee947
Prashraya Panta <prashrayapanta33@gmail.com>
2025-06-25 01:40:36 +0545

    Swiper auto slider

--- src/components/ui/swiper.jsx
+++ src/components/ui/swiper.jsx
@@ -1,24 +1,25 @@
-
 // import Swiper core and required modules
-import { Navigation , Pagination} from 'swiper/modules';
+import { Navigation, Autoplay } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-
-
-import 'swiper/css/pagination';
+import "swiper/css/autoplay";
 
 function SwiperComponent() {
   return (
     <Swiper
       spaceBetween={20} // Adjust spacing between slides
-      slidesPerView={2} // Show one slide at a time
+      slidesPerView={2} // Show two slides at a time
       loop={true} // Enable looping of slides
       navigation={true} // Enable navigation buttons
-      modules={[Navigation]} // Include the Navigation module
+      autoplay={{
+        delay: 3000, // Delay between slides in milliseconds (3 seconds)
+        disableOnInteraction: false, // Autoplay continues even after user interaction
+      }}
+      modules={[Navigation, Autoplay]} // Include Navigation and Autoplay modules
       onSlideChange={() => console.log("Slide changed")}
       onSwiper={(swiper) => console.log(swiper)}
     >
@@ -31,7 +32,6 @@ function SwiperComponent() {
       <SwiperSlide>
         <img src="../../../slider-3.jpg" alt="Slide 3" className="w-full h-auto" />
       </SwiperSlide>
-      
     </Swiper>
   );
 }
